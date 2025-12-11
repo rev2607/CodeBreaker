f0f182853eca711c2c98d7cfc0e41e183822099b
Revanth Guthula <revanthg2607@gmail.com>
2025-12-11 22:48:35 +0530

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by revanth on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
