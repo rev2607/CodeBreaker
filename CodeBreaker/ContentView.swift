0ca6ca1aad06f941f103de828da7c2de633364f5
Revanth Guthula <revanthg2607@gmail.com>
2025-12-15 21:23:57 +0530

    pegs function

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            pegs(colors: [.red, .green, .green, .yellow])
+            pegs(colors: [.red, .blue, .green, .red])
+            pegs(colors: [.red, .yellow, .green, .blue])
         }
         .padding()
+    }
+    
+    func pegs(colors: Array<Color>) -> some View{
+        HStack {
+            ForEach(colors.indices, id: \.self) {index in
+                RoundedRectangle(cornerRadius:10)
+                    .aspectRatio(1,contentMode: .fit)
+                    .foregroundStyle(colors[index])
+            }
+            MatchMarkers(matches: [.exact, .inexact, .nomatch, .exact])
+        }
     }
 }
 
